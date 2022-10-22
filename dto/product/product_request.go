0718451b7e3d75a7931ca1f9d3cd801f8d9043f7
IrhamFR <irham.f.rufdo@gmail.com>
2022-10-23 01:42:06 +0700

    update

--- dto/product/product_request.go
+++ dto/product/product_request.go
@@ -2,11 +2,11 @@ package productdto
 
 type ProductRequest struct {
 	Name  string `json:"name" form:"name" gorm:"type: varchar(255)" validate:"required"`
-	Image string `json:"image" form:"image" gorm:"type: varchar(255)"`
+	Image string `json:"image" form:"image" gorm:"type: varchar(255)" validate:"required"`
 	// Desc       string `json:"desc" gorm:"type:text" form:"desc" validate:"required"`
-	Buy        int   `json:"buy" form:"buy" gorm:"type: int" validate:"required"`
-	Sell       int   `json:"sell" form:"sell" gorm:"type: int" validate:"required"`
-	Qty        int   `json:"qty" form:"qty" gorm:"type: int" validate:"required"`
+	Buy        int   `json:"buy" form:"buy" gorm:"type: int"`
+	Sell       int   `json:"sell" form:"sell" gorm:"type: int"`
+	Qty        int   `json:"qty" form:"qty" gorm:"type: int"`
 	UserID     int   `json:"user_id" form:"user_id"`
 	CategoryID []int `json:"category_id" form:"category_id" gorm:"type: int"`
 }
